b286da160b2dc68431a6d5c7b63a82b9c7133b6e
Ravindra Shekhawat <masterravi@users.noreply.github.com>
2020-09-16 13:29:32 +0530

    Updated with changing endpoint

--- app/src/main/java/com/training/coroutinepackdemo/data/api/ApiService.kt
+++ app/src/main/java/com/training/coroutinepackdemo/data/api/ApiService.kt
@@ -1,14 +1,16 @@
 package com.training.coroutinepackdemo.data.api
 
 import com.training.coroutinepackdemo.data.local.entity.NewsEntity
+import com.training.coroutinepackdemo.data.models.NewsResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("news/{pageId}")
-    suspend fun getNews(@Path("pageId") pageId: String):List<NewsEntity>
+    @GET("top-headlines")
+    suspend fun getNews(@Query("page") pageId: String, @Query("apiKey") apiKey: String, @Query("country") country: String):NewsResponse
 
-    @GET("more-news")
+    @GET("more-newsList")
     suspend fun moreNews():List<NewsEntity>
 }
