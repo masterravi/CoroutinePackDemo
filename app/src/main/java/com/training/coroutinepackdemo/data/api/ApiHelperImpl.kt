cf4f0d0470f64294d2113bb8ec815092370f7287
Ravindra Shekhawat <masterravi@users.noreply.github.com>
2020-09-16 13:03:08 +0530

    Updated ApiHelper for UI changes

--- app/src/main/java/com/training/coroutinepackdemo/data/api/ApiHelperImpl.kt
+++ app/src/main/java/com/training/coroutinepackdemo/data/api/ApiHelperImpl.kt
@@ -1,10 +1,12 @@
 package com.training.coroutinepackdemo.data.api
 
 import com.training.coroutinepackdemo.data.local.entity.NewsEntity
+import com.training.coroutinepackdemo.data.models.NewsResponse
+import com.training.coroutinepackdemo.utils.Constants
 
 class  ApiHelperImpl(private val apiService: ApiService):ApiHelper{
 
-    override suspend fun getNews(pageId:String):List<NewsEntity> = apiService.getNews(pageId)
+    override suspend fun getNews(pageId:String):NewsResponse = apiService.getNews(pageId,Constants.API_KEY,Constants.country)
 
     override suspend fun getMoreNews():List<NewsEntity> = apiService.moreNews()
 }
