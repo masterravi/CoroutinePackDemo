15c8474d8e75dbb362e0489dfd60e44caa1c5758
Ravindra Shekhawat <masterravi@users.noreply.github.com>
2020-09-16 13:02:01 +0530

    Update response type for APi Helper

--- app/src/main/java/com/training/coroutinepackdemo/data/api/ApiHelper.kt
+++ app/src/main/java/com/training/coroutinepackdemo/data/api/ApiHelper.kt
@@ -1,10 +1,11 @@
 package com.training.coroutinepackdemo.data.api
 
 import com.training.coroutinepackdemo.data.local.entity.NewsEntity
+import com.training.coroutinepackdemo.data.models.NewsResponse
 
 interface  ApiHelper{
 
-    suspend fun getNews(pageId:String):List<NewsEntity>
+    suspend fun getNews(pageId:String): NewsResponse
 
     suspend fun getMoreNews():List<NewsEntity>
 }
