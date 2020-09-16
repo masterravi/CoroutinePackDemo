39ed49d3f76d527da807a093733ac565c574ddeb
Ravindra Shekhawat <masterravi@users.noreply.github.com>
2020-09-16 13:57:39 +0530

    Update NewsListRepositoryImpl.kt

--- app/src/main/java/com/training/coroutinepackdemo/data/repository/NewsListRepositoryImpl.kt
+++ app/src/main/java/com/training/coroutinepackdemo/data/repository/NewsListRepositoryImpl.kt
@@ -3,15 +3,33 @@ package com.training.coroutinepackdemo.data.repository
 import com.training.coroutinepackdemo.data.api.ApiHelper
 import com.training.coroutinepackdemo.data.local.dao.NewsDaoImpl
 import com.training.coroutinepackdemo.data.local.entity.NewsEntity
+import com.training.coroutinepackdemo.data.models.News
 import com.training.coroutinepackdemo.utils.NetworkHelper
 
 class NewsListRepositoryImpl(private val apiHelper: ApiHelper, private val networkHelper: NetworkHelper, private val databaseService: NewsDaoImpl):NewsListRepository{
 
     override suspend fun getNews(pageId:String):List<NewsEntity>{
-        if(networkHelper.isNetworkConnected())
-            return apiHelper.getNews(pageId)
-        else
-            return databaseService.getAll()
+        val newsList= mutableListOf<NewsEntity>()
+        if(networkHelper.isNetworkConnected()){
+            var newsResponse=  apiHelper.getNews(pageId)
+            if(newsResponse.totalResults>0){
+                newsResponse.newsList?.forEach {
+                    val newsEntity= NewsEntity(
+                        id = 0,
+                        title = it.title,
+                        description = it.description,
+                        urlToImage = it.urlToImage,
+                        source = it.source.name,
+                        url = it.url
+                    )
+                    newsList.add(newsEntity)
+                }
+            }
+            return  newsList
+        }else{
+            return  databaseService.getAll()
+        }
+
     }
 
     override suspend fun getMoreNews():List<NewsEntity> = apiHelper.getMoreNews()
